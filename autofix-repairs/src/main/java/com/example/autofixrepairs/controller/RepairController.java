10b9601b7a7211d8005252f678cccfce0ed05b92
Isidora <isidora.oyanedel@usach.cl>
2024-06-07 00:51:19 -0400

    aaaaaaa

--- autofix-repairs/src/main/java/com/example/autofixrepairs/controller/RepairController.java
+++ autofix-repairs/src/main/java/com/example/autofixrepairs/controller/RepairController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/repair")
+@RequestMapping("/api/repairs")
 public class RepairController {
     @Autowired
     RepairService repairService;
--- autofix-repairs/src/main/java/com/example/autofixrepairs/entity/Repair.java
+++ autofix-repairs/src/main/java/com/example/autofixrepairs/entity/Repair.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "repair")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
